5c0f061233cf5d693ee311dd43f8c88eeb7c23d4
bique14 <peerasorn14@gmail.com>
2018-04-23 17:36:46 +0700

    update schema

--- demo-433-transmit/demo-433-transmit.ino
+++ demo-433-transmit/demo-433-transmit.ino
@@ -1,20 +1,30 @@
+/*  ________________________________________
+ *  | Arduino Uno  |      433 Trasmitter    | 
+ *  |--------------|------------------------|
+ *  |   5v         |         VCC            | 
+ *  |   Data       |         D4             |
+ *  |   GND        |         GND            |
+ *  |--------------|------------------------|
+*/
+
+
 #define rfTransmitPin 4  //RF Transmitter pin = digital pin 4
- #define ledPin 13        //Onboard LED = digital pin 13
+#define ledPin 13        //Onboard LED = digital pin 13
 
- void setup(){
-   pinMode(rfTransmitPin, OUTPUT);     
-   pinMode(ledPin, OUTPUT);    
- }
+void setup(){
+  pinMode(rfTransmitPin, OUTPUT);     
+  pinMode(ledPin, OUTPUT);    
+}
 
- void loop(){
-   for(int i=4000; i>5; i=i-(i/3)){
-     digitalWrite(rfTransmitPin, HIGH);     //Transmit a HIGH signal
-     digitalWrite(ledPin, HIGH);            //Turn the LED on
-     delay(2000);                           //Wait for 1 second
-     
-     digitalWrite(rfTransmitPin,LOW);      //Transmit a LOW signal
-     digitalWrite(ledPin, LOW);            //Turn the LED off
-     delay(i);                            //Variable delay
-   }
- }
+void loop(){
+  for(int i=4000; i>5; i=i-(i/3)){
+    digitalWrite(rfTransmitPin, HIGH);    
+    digitalWrite(ledPin, HIGH);           
+    delay(2000);                          
+    
+    digitalWrite(rfTransmitPin,LOW);      
+    digitalWrite(ledPin, LOW);            
+    delay(i);                            
+  }
+}
 
